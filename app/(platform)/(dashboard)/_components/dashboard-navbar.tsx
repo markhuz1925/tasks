11e0afe2da99821cf5ed07210d68581039a1a180
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-14 19:38:39 +0800

    Build DashboardNavbar component.

---
+++ app/(platform)/(dashboard)/_components/dashboard-navbar.tsx
@@ -0,0 +1,55 @@
+import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { PlusIcon } from "lucide-react";
+
+export default function DashboardNavbar() {
+  return (
+    <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
+      {/* TODO: mobile view */}
+      {/* desktop view */}
+      <div className="flex items-center gap-x-4">
+        <div className="hidden md:flex">
+          <Logo />
+        </div>
+        <Button
+          size="sm"
+          className="rounded-sm hidden md:block h-auto py-1.5 px-2"
+        >
+          Create
+        </Button>
+        <Button size="sm" className="rounded-sm block md:hidden">
+          <PlusIcon className="w-4 h-4" />
+        </Button>
+      </div>
+      <div className="ml-auto flex items-center gap-x-2">
+        <OrganizationSwitcher
+          hidePersonal
+          afterCreateOrganizationUrl="/organization/:id"
+          afterSelectOrganizationUrl="/organization/:id"
+          afterLeaveOrganizationUrl="/select-org"
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              },
+            },
+          }}
+        />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: {
+                height: 30,
+                width: 30,
+              },
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
+}
