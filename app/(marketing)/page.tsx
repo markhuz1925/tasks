66f0e196a71444cdb674efd2ac69b8da9730b2b0
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-14 17:44:49 +0800

    Build Navbar component.

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -30,7 +30,7 @@ export default function MarketingPage() {
         <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
           Tasks! helps team move
         </h1>
-        <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
+        <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit -skew-y-3">
           work forward.
         </div>
       </div>
