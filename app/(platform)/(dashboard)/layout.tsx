606715eb650730644a275e6bcd61ac2dd3280dea
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-14 19:19:37 +0800

    Create Dashboard page.

---
+++ app/(platform)/(dashboard)/layout.tsx
@@ -0,0 +1,14 @@
+import DashboardNavbar from "./_components/dashboard-navbar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="h-full ">
+      <DashboardNavbar />
+      {children}
+    </div>
+  );
+}
---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,3 @@
+export default function OrganizationIdPage() {
+  return <div>OrganizationIdPage</div>;
+}
