7a697e83397aa2bf69ce09823af43a2bcf7576b2
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-14 16:09:59 +0800

    Add and apply custom font.

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,28 @@
 import { Button } from "@/components/ui/button";
 import { MedalIcon } from "lucide-react";
 import Link from "next/link";
+import localFont from "next/font/local";
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const headingFont = localFont({
+  src: "../../public/fonts/font.woff2",
+});
+
+const textFont = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function MarketingPage() {
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="flex flex-col items-center justify-center">
+      <div
+        className={cn(
+          "flex flex-col items-center justify-center",
+          headingFont.className
+        )}
+      >
         <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
           <MedalIcon className="h-6 w-6 mr-2" />
           No 1 Task management!
@@ -17,7 +34,12 @@ export default function MarketingPage() {
           work forward.
         </div>
       </div>
-      <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+      <div
+        className={cn(
+          "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+          textFont.className
+        )}
+      >
         Collaborate, manage projects, and reach new productivity peaks. From
         high rises to the home office, the way your team works is unique —
         accomplish it all with Tasks!
