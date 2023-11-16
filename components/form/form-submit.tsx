47fd6ddbb554358627f5021fbb7de95cfeaa9735
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-16 14:23:07 +0800

    Create FormSubmit component.

---
+++ components/form/form-submit.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+import { Button } from "@/components/ui/button";
+
+interface Props {
+  children: React.ReactNode;
+  disabled?: boolean;
+  className?: string;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "primary"
+    | "ghost"
+    | "link";
+}
+
+export default function FormSubmit({
+  children,
+  disabled,
+  className,
+  variant,
+}: Props) {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      type="submit"
+      disabled={disabled || pending}
+      className={className}
+      variant={variant}
+    >
+      {children}
+    </Button>
+  );
+}
