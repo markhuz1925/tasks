4cb7af529820116bc4948e6284d0b350a2af2c1a
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-16 15:14:26 +0800

    Build OrganizationId page.

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,3 +1,15 @@
+import { Separator } from "@/components/ui/separator";
+import Info from "./_component/info";
+import BoardList from "./_component/board-list";
+
 export default function OrganizationIdPage() {
-  return <div>OrganizationIdPage</div>;
+  return (
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
+    </div>
+  );
 }
