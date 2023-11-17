73f6f13ad3c68eb43082de559d12860ce3cf5665
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-17 13:06:54 +0800

    Update the board title.

--- app/(platform)/(dashboard)/board/[boardId]/_component/board-title-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_component/board-title-form.tsx
@@ -1,14 +1,38 @@
 "use client";
 
+import { updateBoard } from "@/actions/update-board";
 import { FormInput } from "@/components/form/form-input";
 import { Button } from "@/components/ui/button";
+import { useAction } from "@/hooks/use-action";
 import { Board } from "@prisma/client";
+import { AlertCircleIcon, CheckCircle2 } from "lucide-react";
 import { ElementRef, useRef, useState } from "react";
+import { toast } from "sonner";
 
 export function BoardTitleForm({ data }: { data: Board }) {
+  const [title, setTitle] = useState(data.title);
   const [isEditing, setIsEditing] = useState(false);
   const formRef = useRef<ElementRef<"form">>(null);
   const inputRef = useRef<ElementRef<"input">>(null);
+  const { execute } = useAction(updateBoard, {
+    onSuccess: (data) => {
+      toast(
+        <div className="flex items-center gap-x-2">
+          <CheckCircle2 className="w-4 h-4 text-green-500" />
+          {`Board ${data.title} updated!`}
+        </div>
+      );
+      setTitle(data.title);
+      disableEditing();
+    },
+    onError: (error) => {
+      toast(
+        <div className="flex items-center gap-x-2">
+          <AlertCircleIcon className="w-4 h-4 text-red-500" /> {error}
+        </div>
+      );
+    },
+  });
 
   const enableEditing = () => {
     setIsEditing(true);
@@ -26,7 +50,7 @@ export function BoardTitleForm({ data }: { data: Board }) {
     const title = formData.get("title") as string;
     if (!title) return;
 
-    console.table({ title });
+    execute({ title, id: data.id });
   };
 
   const onBlur = () => {
@@ -43,7 +67,7 @@ export function BoardTitleForm({ data }: { data: Board }) {
         <FormInput
           id="title"
           ref={inputRef}
-          defaultValue={data.title}
+          defaultValue={title}
           onBlur={onBlur}
           className="text-lg font-bold px-[7px] py-1 h-7 w-full bg-transparent border-none"
         />
@@ -57,7 +81,7 @@ export function BoardTitleForm({ data }: { data: Board }) {
       variant="transparent"
       className="font-bold text-lg w-auto h-auto p-1 px-2"
     >
-      {data?.title}
+      {title}
     </Button>
   );
 }
