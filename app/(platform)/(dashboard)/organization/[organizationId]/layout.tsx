b050e1e1c80c0950de43b33963dff772484f0c73
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-17 10:28:15 +0800

    Set browser title to selected organization.

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,14 @@
+import { auth } from "@clerk/nextjs";
 import { OrgControl } from "./_component/org-control";
+import { startCase } from "lodash";
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "Organization"),
+  };
+}
 
 export default function OrganizationIdLayout({
   children,
