3a0a75567075ba8ff4b2d6d0c2a801857c598256
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-17 11:17:32 +0800

    Build BoardId layout.

---
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,59 @@
+import prisma from "@/prisma/client";
+import { auth } from "@clerk/nextjs";
+import { startCase } from "lodash";
+import { notFound, redirect } from "next/navigation";
+import BoardNavbar from "./_component/board-navbar";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { boardId: string };
+}) {
+  const { orgId } = auth();
+  if (!orgId)
+    return {
+      title: "Board",
+    };
+
+  const board = await prisma.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  return {
+    title: startCase(board?.title || "Board"),
+  };
+}
+
+export default async function BoardIdLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { boardId: string };
+}) {
+  const { orgId } = auth();
+  if (!orgId) return redirect("/select-org");
+
+  const board = await prisma.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  if (!board) notFound();
+
+  return (
+    <div
+      className="relative h-full bg-no-repeat bg-cover bg-center"
+      style={{ backgroundImage: `url(${board.imageFullUrl})` }}
+    >
+      <div className="absolute inset-0 bg-black/10" />
+      <BoardNavbar id={params.boardId} />
+      <main className="relative pt-28 h-full">{children}</main>
+    </div>
+  );
+}
