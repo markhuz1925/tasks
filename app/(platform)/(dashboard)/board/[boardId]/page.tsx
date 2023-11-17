ab1ac7c45942be520280f7e0f579469771ec3fa1
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-17 19:45:45 +0800

    Render Lists

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,3 +1,39 @@
-export default function BoardIdPage() {
-  return <div>BoardIdPage</div>;
+import prisma from "@/prisma/client";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { ListContainer } from "./_component/list-container";
+
+export default async function BoardIdPage({
+  params,
+}: {
+  params: { boardId: string };
+}) {
+  const { orgId } = auth();
+
+  if (!orgId) return redirect("/select-org");
+
+  const list = await prisma.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+
+  return (
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer boardId={params.boardId} data={list} />
+    </div>
+  );
 }
