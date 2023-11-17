3d29aff4fb17c08cf7f0dd514ee21ed5c9b899d3
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-17 10:11:05 +0800

    Render boards per organization.

--- app/(platform)/(dashboard)/organization/[organizationId]/_component/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_component/board-list.tsx
@@ -1,8 +1,27 @@
 import { FormPopover } from "@/components/form/form-popover";
 import { Hint } from "@/components/hint";
+import prisma from "@/prisma/client";
+import { auth } from "@clerk/nextjs";
 import { HelpCircleIcon, User2Icon } from "lucide-react";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function BoardList() {
+export default async function BoardList() {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return redirect("/select-org");
+  }
+
+  const boards = await prisma.board.findMany({
+    where: {
+      orgId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="space-y-4">
       <div className="flex items-center font-semibold text-lg text-neutral-700">
@@ -10,6 +29,17 @@ export default function BoardList() {
         Your boards
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        {boards.map((board) => (
+          <Link
+            key={board.id}
+            href={`/board/${board.id}`}
+            className="group relative aspect-video bg-no-repeat bg-center bg-cover rounded-sm bg-sky-700 w-full h-full p-2 overflow-hidden"
+            style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
+          >
+            <div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition" />
+            <p className="relative font-semibold text-white">{board.title}</p>
+          </Link>
+        ))}
         <FormPopover sideOffset={10} side="right">
           <div
             role="button"
