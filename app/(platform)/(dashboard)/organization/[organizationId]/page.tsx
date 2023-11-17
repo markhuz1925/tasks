78a3cf39c61afa95e35ede53d3389d85257992b9
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-17 10:15:49 +0800

    Use Suspense component from react.

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,6 +1,7 @@
 import { Separator } from "@/components/ui/separator";
 import Info from "./_component/info";
 import BoardList from "./_component/board-list";
+import { Suspense } from "react";
 
 export default function OrganizationIdPage() {
   return (
@@ -8,7 +9,9 @@ export default function OrganizationIdPage() {
       <Info />
       <Separator className="my-4" />
       <div className="px-2 md:px-4">
-        <BoardList />
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
       </div>
     </div>
   );
