c974ee56f566b3b1756570eda0672fbb9c86201b
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-19 02:06:41 +0800

    Creating a Card.

--- app/(platform)/(dashboard)/board/[boardId]/_component/card-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_component/card-form.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import { createCard } from "@/actions/create-card";
 import { FormSubmit } from "@/components/form/form-submit";
 import { FormTextarea } from "@/components/form/form-textarea";
 import { Button } from "@/components/ui/button";
-import { PlusIcon, XIcon } from "lucide-react";
-import { forwardRef } from "react";
+import { useAction } from "@/hooks/use-action";
+import { AlertCircleIcon, CheckCircle2, PlusIcon, XIcon } from "lucide-react";
+import { useParams } from "next/navigation";
+import { ElementRef, KeyboardEventHandler, forwardRef, useRef } from "react";
+import { toast } from "sonner";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
 
 interface Props {
   listId: string;
@@ -15,16 +20,66 @@ interface Props {
 
 export const CardForm = forwardRef<HTMLTextAreaElement, Props>(
   ({ listId, isEditing, enableEditing, disableEditing }, ref) => {
+    const params = useParams();
+    const formRef = useRef<ElementRef<"form">>(null);
+
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") disableEditing();
+    };
+
+    useEventListener("keydown", onKeyDown);
+    useOnClickOutside(formRef, disableEditing);
+
+    const onTextareaKeyDown: KeyboardEventHandler<HTMLTextAreaElement> = (
+      e
+    ) => {
+      if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+        formRef.current?.requestSubmit();
+      }
+    };
+
+    const { execute, fieldErrors } = useAction(createCard, {
+      onSuccess: (data) => {
+        toast(
+          <div className="flex items-center gap-x-2">
+            <CheckCircle2 className="w-4 h-4 text-green-500" />
+            {`Card ${data.title} created!`}
+          </div>
+        );
+        formRef.current?.reset();
+        disableEditing();
+      },
+      onError: (error) => {
+        toast(
+          <div className="flex items-center gap-x-2">
+            <AlertCircleIcon className="w-4 h-4 text-red-500" /> {error}
+          </div>
+        );
+      },
+    });
+
+    const onSubmit = (formData: FormData) => {
+      const title = formData.get("title") as string;
+      const boardId = params.boardId as string;
+
+      execute({ title, listId, boardId });
+    };
+
     if (isEditing) {
       return (
-        <form className="m-1 py-0.5 px-1 space-y-4">
+        <form
+          ref={formRef}
+          action={onSubmit}
+          className="m-1 py-0.5 px-1 space-y-4"
+        >
           <FormTextarea
             ref={ref}
             id="title"
-            onKeyDown={() => {}}
+            onKeyDown={onTextareaKeyDown}
             placeholder="Enter a title for this card"
+            errors={fieldErrors}
           />
-          <input hidden id={listId} name={listId} value={listId} />
           <div className="flex items-center gap-x-1">
             <FormSubmit>Add Card</FormSubmit>
             <Button onClick={disableEditing} size="sm" variant="ghost">
