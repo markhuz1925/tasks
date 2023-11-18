a681d30b1ec8e46b6c9b6ed20b6bc5d548372ccc
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-18 18:57:36 +0800

    Updating the List title.

--- app/(platform)/(dashboard)/board/[boardId]/_component/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_component/list-header.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { updateList } from "@/actions/update-list";
 import { FormInput } from "@/components/form/form-input";
+import { useAction } from "@/hooks/use-action";
 import { List } from "@prisma/client";
+import { AlertCircleIcon, CheckCircle2 } from "lucide-react";
 import { ElementRef, useRef, useState } from "react";
+import { toast } from "sonner";
 import { useEventListener } from "usehooks-ts";
 
 export function ListHeader({ data }: { data: List }) {
@@ -27,22 +31,57 @@ export function ListHeader({ data }: { data: List }) {
     if (e.key === "Escape") formRef.current?.requestSubmit();
   };
 
+  const { execute } = useAction(updateList, {
+    onSuccess: (data) => {
+      toast(
+        <div className="flex items-center gap-x-2">
+          <CheckCircle2 className="w-4 h-4 text-green-500" />
+          {`Renamed to ${data.title}.`}
+        </div>
+      );
+      setTitle(data.title);
+      disableEditing();
+    },
+    onError: (error) => {
+      toast(
+        <div className="flex items-center gap-x-2">
+          <AlertCircleIcon className="w-4 h-4 text-red-500" /> {error}
+        </div>
+      );
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    if (title === data.title) return disableEditing();
+
+    execute({ title, id, boardId });
+  };
+
+  const onBlur = () => {
+    formRef.current?.requestSubmit();
+  };
+
   useEventListener("keydown", onKeyDown);
 
   return (
     <div className="pt-2 px-2 text-sm font-semibold flex items-start justify-between gap-x-2">
       {isEditing ? (
-        <form className="flex-1 px-[2px]">
+        <form action={onSubmit} ref={formRef} className="flex-1 px-[2px]">
           <input hidden id="id" name="id" value={data.id} />
           <input hidden id="boardId" name="boardId" value={data.boardId} />
           <FormInput
             ref={inputRef}
-            onBlur={() => {}}
+            onBlur={onBlur}
             id="title"
             placeholder="Enter list title..."
             defaultValue={title}
             className="text-sm px-[7px] py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transistion truncate bg-transparent focus:bg-white"
           />
+          <button type="submit" hidden />
         </form>
       ) : (
         <div
