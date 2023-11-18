e06035934524c37785d5e852fc49d3a547a82b85
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-11-19 01:30:52 +0800

    Update ListItem component.

--- app/(platform)/(dashboard)/board/[boardId]/_component/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_component/list-item.tsx
@@ -1,5 +1,7 @@
 import { ListWithCards } from "@/types";
 import { ListHeader } from "./list-header";
+import { ElementRef, useRef, useState } from "react";
+import { CardForm } from "./card-form";
 
 export function ListItem({
   index,
@@ -8,10 +10,31 @@ export function ListItem({
   index: number;
   data: ListWithCards;
 }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const textareaRef = useRef<ElementRef<"textarea">>(null);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      textareaRef.current?.focus();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
   return (
     <li className="shrink-0 w-[272px] h-full select-none">
       <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
-        <ListHeader data={data} />
+        <ListHeader onAddCard={enableEditing} data={data} />
+        <CardForm
+          ref={textareaRef}
+          listId={data.id}
+          isEditing={isEditing}
+          enableEditing={enableEditing}
+          disableEditing={disableEditing}
+        />
       </div>
     </li>
   );
