50f0bd6d18606fe042a8cafd0de059da6e6a24bf
Marko Luther Antiola <antiolamarkoluther@gmail.com>
2023-12-06 22:51:01 +0800

    Make title a optional type.

--- actions/update-card/schema.ts
+++ actions/update-card/schema.ts
@@ -2,12 +2,14 @@ import { z } from "zod";
 
 export const UpdateCard = z.object({
   boardId: z.string(),
-  title: z
-    .string({
-      required_error: "Title is required",
-      invalid_type_error: "Title is required",
-    })
-    .min(3, "Title must be at least 3 characters"),
+  title: z.optional(
+    z
+      .string({
+        required_error: "Title is required",
+        invalid_type_error: "Title is required",
+      })
+      .min(3, "Title must be at least 3 characters")
+  ),
   description: z.optional(
     z
       .string({
